10f5ef48634b0fb559e9a3b0c86293d2bf84c1ec
Marius D. Pascariu <rpascariu@outlook.com>
2021-03-14 19:18:02 +0100

    v0.0.2 - Add wpp2019 LT data in the pack

---
+++ R/MortalityCauses-data.R
@@ -0,0 +1,19 @@
+# --------------------------------------------------- #
+# Author: Marius D. PASCARIU
+# License: GNU General Public License v3.0
+# Last update: Sun Mar 14 18:48:15 2021
+# --------------------------------------------------- #
+
+
+#' Abridged life table, for both sexes combined, by region, 
+#' subregion and country, 2015-2020
+#' 
+#' @source 
+#' United Nations, Department of Economic and Social Affairs, 
+#' Population Division (2019). World Population Prospects 2019,
+#' \href{https://population.un.org/wpp/Download/Standard/Mortality/}{
+#'  Online Edition. Rev. 1.}
+#' 
+#' @examples 
+#' data_wpp2019_lt
+"data_wpp2019_lt"
---
+++ R/MortalityCauses-package.R
@@ -0,0 +1,16 @@
+# --------------------------------------------------- #
+# Author: Marius D. PASCARIU
+# License: GNU General Public License v3.0
+# Last update: Sun Mar 14 19:00:25 2021
+# --------------------------------------------------- #
+
+# MortalityCauses Package
+
+#' @details
+#' To learn more about the package, start with the vignettes:
+#' \code{browseVignettes(package = "MortalityCauses")}
+#'
+#' @name MortalityCauses
+#' @docType package
+"_PACKAGE"
+ 
