f30a6a3b48512805f77fed6c63cbc0b951aae6a6
Marius D. PASCARIU <rpascariu@outlook.com>
2022-03-15 17:37:43 +0100

    v0.10.4 - remove the white background color from logos

--- inst/app/www/addNavLink.js
+++ inst/app/www/addNavLink.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".navbar .container-fluid .navbar-collapse").append('<ul class="nav navbar-nav navbar-right"><li><a class="nav-image" href="https://demography.cass.anu.edu.au/" target="_blank"><img src="www/LogoANU.png" height="40px"</a></li></ul>');
-  $(".navbar .container-fluid .navbar-collapse").append('<ul class="nav navbar-nav navbar-right"><li><a class="nav-image" href="https://www.demographicscience.ox.ac.uk/" target="_blank"><img src="www/LogoOxford.png" height="40px"</a></li></ul>');
-  $(".navbar .container-fluid .navbar-collapse").append('<ul class="nav navbar-nav navbar-right"><li><a class="nav-image" href="https://www.sdu.dk/en/om_sdu/institutter_centre/cpop" target="_blank"><img src="www/LogoSDU.png" height="40px"</a></li></ul>');
+  $(".navbar .container-fluid .navbar-collapse").append('<ul class="nav navbar-nav navbar-right"><li><a class="nav-image" href="https://demography.cass.anu.edu.au/" target="_blank"><img src="www/logo_anu.png" height="40px"</a></li></ul>');
+  $(".navbar .container-fluid .navbar-collapse").append('<ul class="nav navbar-nav navbar-right"><li><a class="nav-image" href="https://www.demographicscience.ox.ac.uk/" target="_blank"><img src="www/logo_oxford.png" height="40px"</a></li></ul>');
+  $(".navbar .container-fluid .navbar-collapse").append('<ul class="nav navbar-nav navbar-right"><li><a class="nav-image" href="https://www.sdu.dk/en/om_sdu/institutter_centre/cpop" target="_blank"><img src="www/logo_sdu.png" height="40px"</a></li></ul>');
 });
