003cf9da67b4a528f97feb655bc8e8e31080a6f1
Doug Leasure <doug.leasure@protonmail.com>
2022-04-26 20:13:17 +0100

    build lemur from github

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,7 +7,7 @@ sudo systemctl reload nginx
 # shinyproxy
 docker stop shinyproxy
 docker rm shinyproxy
-docker build --tag shinyproxy $HOME/git/lemur/deploy/
+docker build --tag shinyproxy $HOME/git/lemur/deploy/shinyproxy
 
 docker run -d -v /var/run/docker.sock:/var/run/docker.sock --restart=always --group-add $(getent group docker | cut -d: -f3) --net shiny-net -p 127.0.0.1:8080:8080 --name shinyproxy shinyproxy
 
